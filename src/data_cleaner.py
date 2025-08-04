d1da4a1ea2b32f722beff2d57515e613ee5e9c51
eli_shteinman <a0533057932@gmail.com>
2025-08-04 17:57:33 +0300

    Refactor code structure for improved readability and maintainability

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -1,17 +1,44 @@
 import pandas as pd
+import string
 
 
 class DataCleaner:
-    def __init__(
-        self,
-        columns_to_drop,
-    ):
-        self.columns_to_drop = columns_to_drop or []
-        
-    def clean(self, data: pd.DataFrame) -> pd.DataFrame:
-        cleaned_data = data.copy()
-        if self.columns_to_drop:
-            cleaned_data = cleaned_data.drop(
-                columns=self.columns_to_drop, errors="ignore"
+    @staticmethod
+    def delete_unclassified(
+        data: pd.DataFrame, columns_to_delete_unclassified: list[str]
+    ) -> pd.DataFrame:
+        delete_data = data.copy()
+        delete_data = delete_data.dropna(subset=columns_to_delete_unclassified)
+        return delete_data
+
+    @staticmethod
+    def convert_to_lowercase(
+        data: pd.DataFrame, coloms_to_lowercase: list[str]
+    ) -> pd.DataFrame:
+        data = data.copy()
+        for col in coloms_to_lowercase:
+            data[col] = DataCleaner._conversion(data[col])
+        return data
+
+    @staticmethod
+    def _conversion(ser: pd.Series) -> pd.Series:
+        return ser.map(lambda t: t.lower())
+
+    @staticmethod
+    def removing_punctuation_marks(
+        data: pd.DataFrame, coloms_to_removing_punctuation_marks: list[str]
+    ) -> pd.DataFrame:
+        data = data.copy()
+        for col in coloms_to_removing_punctuation_marks:
+            data[col] = data[col].map(
+                lambda x: x.translate(str.maketrans("", "", string.punctuation))
             )
+        return data
+
+    @staticmethod
+    def deleting_columns(
+        data: pd.DataFrame, columns_to_drop: list[str]
+    ) -> pd.DataFrame:
+        cleaned_data = data.copy()
+        cleaned_data = cleaned_data.drop(columns=columns_to_drop, errors="ignore")
         return cleaned_data
--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
+import pandas as pd
 from data_loader import DataHandler
 from data_cleaner import DataCleaner
-full_path = '/Users/lyhwstynmn/פרוייקטים/python/test/data/tweets_dataset.csv'
+
+full_path = "/Users/lyhwstynmn/פרוייקטים/python/test/data/tweets_dataset.csv"
 loader = DataHandler(full_path)
 row_data = loader.load_data()
 columns_to_drop = []
 for col in row_data.columns:
-    if col not in {'Text', 'Biased'}:
+    if col not in {"Text", "Biased"}:
         columns_to_drop.append(col)
-cleaner = DataCleaner(columns_to_drop)
-clean_data = cleaner.clean(row_data)
+cleaner = DataCleaner()
+delete_unclassified = cleaner.delete_unclassified(row_data, ["Text"])
+removing_punctuation_marks = cleaner.removing_punctuation_marks(
+    delete_unclassified, ["Text"]
+)
+convert_to_lowercase = cleaner.convert_to_lowercase(
+    removing_punctuation_marks, ["Text"]
+)
+
+clean_data = cleaner.deleting_columns(convert_to_lowercase, columns_to_drop)
+
+clean_data.to_csv(
+    "/Users/lyhwstynmn/פרוייקטים/python/test/results/tweets_dataset_cleaned.csv"
+)
